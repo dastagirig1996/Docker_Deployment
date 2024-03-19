21544de9d20619656e96104606db74e5d1adce17
dastagirig1996 <111571182+dastagirig1996@users.noreply.github.com>
2024-03-19 17:30:46 +0530

    Update urls.py

--- api_arms/app1/urls.py
+++ api_arms/app1/urls.py
@@ -5,7 +5,7 @@ from app1.viewsets import (PersonModelViewset, UserViewSet, RoleModelViewset,
     ApiModelViewset)
 from rest_framework.routers import DefaultRouter
 router = DefaultRouter()
-router.register(r'viewset_person', PersonModelViewset, basename='viewset_person')
+# router.register(r'viewset_person', PersonModelViewset, basename='viewset_person')
 router.register(r'user', UserViewSet, basename="user")
 router.register(r'role', RoleModelViewset, basename='role')
 router.register(r'api', ApiModelViewset, basename='api')
