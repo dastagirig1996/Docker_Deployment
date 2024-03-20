07c9487f244e3f073023e2f10f518b00c27b600e
dastagirig1996 <111571182+dastagirig1996@users.noreply.github.com>
2024-03-20 11:02:04 +0530

    Update admin.py

--- api_arms/app1/admin.py
+++ api_arms/app1/admin.py
@@ -1,1 +1,10 @@
 from django.contrib import admin
+from .models import MyUser  # Import your MyUser model here
+
+# Define the admin class for MyUser
+class MyUserAdmin(admin.ModelAdmin):
+    pass  # You can customize the admin options here if needed
+
+# Register MyUser with the admin site
+admin.site.register(MyUser, MyUserAdmin)
+
