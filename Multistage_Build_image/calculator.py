e5ea36504fe7c6a9b469a9368e5179f2b8781e4f
unknown <dastagirig1996@gmail.com>
2024-04-04 09:33:45 +0530

    updated calculator as prime

--- Multistage_Build_image/calculator.py
+++ Multistage_Build_image/calculator.py
@@ -1,39 +1,9 @@
-def add(x, y):
-    return x + y
-
-def subtract(x, y):
-    return x - y
-
-def multiply(x, y):
-    return x * y
-
-def divide(x, y):
-    if y == 0:
-        return "Error! Division by zero."
+n = 50000
+prime = []
+for i in range(1,n+1):
+    for j in range(2,i):
+        if i%j == 0:
+            break
     else:
-        return x / y
-
-print("Select operation:")
-print("1. Add")
-print("2. Subtract")
-print("3. Multiply")
-print("4. Divide")
-
-while True:
-    choice = input("Enter choice (1/2/3/4): ")
-
-    if choice in ('1', '2', '3', '4'):
-        num1 = float(input("Enter first number: "))
-        num2 = float(input("Enter second number: "))
-
-        if choice == '1':
-            print("Result:", add(num1, num2))
-        elif choice == '2':
-            print("Result:", subtract(num1, num2))
-        elif choice == '3':
-            print("Result:", multiply(num1, num2))
-        elif choice == '4':
-            print("Result:", divide(num1, num2))
-        break
-    else:
-        print("Invalid input")
+        prime.append(i)
+print(prime[-10:])            
