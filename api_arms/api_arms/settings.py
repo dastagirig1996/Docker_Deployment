bffaa61f319b0d3d8c72f793c9663537e5fc441f
unknown <dastagirig1996@gmail.com>
2024-04-08 16:48:30 +0530

    mysql configuerd

--- api_arms/api_arms/settings.py
+++ api_arms/api_arms/settings.py
@@ -79,9 +79,17 @@ WSGI_APPLICATION = 'api_arms.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'DB_NAME',
+        'USER': 'DB_USER',
+        'PASSWORD': 'DB_PASSWORD',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
