b89ab74f0d0295df4de4034fbea5c5a28355c6ce
unknown <dastagirig1996@gmail.com>
2024-04-08 17:10:24 +0530

    mysql configurerdd

--- api_arms/api_arms/settings.py
+++ api_arms/api_arms/settings.py
@@ -85,9 +85,9 @@ DATABASES = {
     # }
     'default': {
         'ENGINE': 'django.db.backends.mysql', 
-        'NAME': 'DB_NAME',
-        'USER': 'DB_USER',
-        'PASSWORD': 'DB_PASSWORD',
+        'NAME': 'ajadb',
+        'USER': 'root',
+        'PASSWORD': 'root',
         'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
         'PORT': '3306',
     }
